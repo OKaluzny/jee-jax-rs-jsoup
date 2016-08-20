dad3e7650dc43ab61c33e0ca5545eec250c30f7d
Oleg <cryoengineer@gmail.com>
2016-08-20 12:53:20 +0300

    added test

---
+++ src/test/java/com/kaluzny/CompanyServiceTest.java
@@ -0,0 +1,19 @@
+package com.kaluzny;
+
+import com.kaluzny.nasdaq.company.CompanyService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class CompanyServiceTest {
+
+    @Test
+    public void parseTest() throws IOException {
+        CompanyService parser = new CompanyService();
+        Assert.assertEquals("International Business Machines Corporation \" +\n" +
+                        "                        \"Common Stock Real Time Stock Quotes",
+                parser.getNameCompany("International Business Machines Corporation " +
+                        "Common Stock Real Time Stock Quotes"));
+    }
+}
