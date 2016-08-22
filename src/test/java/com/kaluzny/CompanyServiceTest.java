f6f8159437675985499560125263d2252939a010
Oleg <cryoengineer@gmail.com>
2016-08-22 10:24:10 +0300

    Added some changed

--- src/test/java/com/kaluzny/CompanyServiceTest.java
+++ src/test/java/com/kaluzny/CompanyServiceTest.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class CompanyServiceTest {
 
     @Test
-    public void parseTest() throws IOException {
+    public void getNameCompanyTest() throws IOException {
         CompanyService parser = new CompanyService();
 
         Assert.assertEquals("Nasdaq, Inc. Common Stock Real Time Stock Quotes",
@@ -18,7 +18,7 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void sectorCompanyTest() throws IOException {
+    public void getSectorCompanyTest() throws IOException {
         CompanyService parser = new CompanyService();
         Assert.assertEquals("Industry: Finance",
                 parser.getSectorCompany("International Business Machines Corporation " +
@@ -26,11 +26,26 @@ public class CompanyServiceTest {
     }
 
     @Test
-    public void getDataAsOfTest() throws IOException {
+    public void getStockQuotesTest() throws IOException {
         CompanyService parser = new CompanyService();
         Assert.assertEquals("$70.44",
                 parser.getStockQuotes("International Business Machines Corporation " +
                         "Common Stock Real Time Stock Quotes"));
+    }
 
+    @Test
+    public void getExchangeTest() throws IOException {
+        CompanyService parser = new CompanyService();
+        Assert.assertEquals("Exchange: NASDAQ",
+                parser.getExchange("International Business Machines Corporation " +
+                        "Common Stock Real Time Stock Quotes"));
+    }
+
+    @Test
+    public void getDataAsOfTest() throws IOException {
+        CompanyService parser = new CompanyService();
+        Assert.assertEquals("8/22/2016",
+                parser.getDataAsOf("International Business Machines Corporation " +
+                        "Common Stock Real Time Stock Quotes"));
     }
 }
