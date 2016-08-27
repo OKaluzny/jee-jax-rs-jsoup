9394daad6879cacb4c023ba1c2f4fd829fe12278
Oleg <cryoengineer@gmail.com>
2016-08-27 22:06:55 +0300

    Added change

--- src/main/java/com/kaluzny/nasdaq/company/CompanyService.java
+++ src/main/java/com/kaluzny/nasdaq/company/CompanyService.java
@@ -5,16 +5,17 @@ import com.kaluzny.nasdaq.interceptor.Logged;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 @ApplicationScoped
 @Logged
 public class CompanyService {
 
-    private static final Logger LOGGER = Logger.getLogger(CompanyService.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyService.class);
     private static final String URL_FORMAT = "http://www.nasdaq.com/symbol/%s/real-time";
     private static final String USER_AGENT = "Chrome";
     private static final String EXCHANGE_LABEL = "qbar_exchangeLabel";
@@ -28,7 +29,7 @@ public class CompanyService {
     public Document getDocument(String company) throws IOException {
         String url = String.format(URL_FORMAT, company);
         document = Jsoup.connect(url).userAgent(USER_AGENT).get();
-        LOGGER.info(() -> String.format(">>> Connection to the: '%s'.", company));
+        LOGGER.info( String.format(">>> Connection to the: '%s'.", company));
         return document;
     }
 
@@ -40,7 +41,7 @@ public class CompanyService {
         } catch (IOException exception) {
             throw new CompanyException("Could not parse an exchange.", exception);
         }
-        LOGGER.info(() -> String.format(">>> Gets the name an exchange: '%s'.", element.text()));
+        LOGGER.info(String.format(">>> Gets the name an exchange: '%s'.", element.text()));
         return element.text();
     }
 
@@ -52,7 +53,7 @@ public class CompanyService {
         } catch (IOException exception) {
             throw new CompanyException("Could not parse sector.", exception);
         }
-        LOGGER.info(() -> String.format(">>> Gets the name a sector: '%s'.", element.text()));
+        LOGGER.info( String.format(">>> Gets the name a sector: '%s'.", element.text()));
         return element.text();
     }
 
@@ -64,7 +65,7 @@ public class CompanyService {
         } catch (IOException exception) {
             throw new CompanyException("Could not parse company.", exception);
         }
-        LOGGER.info(() -> String.format(">>> Gets the stock quotes: '%s'.", element.text()));
+        LOGGER.info( String.format(">>> Gets the stock quotes: '%s'.", element.text()));
         return element.text();
     }
 
@@ -76,7 +77,7 @@ public class CompanyService {
         } catch (IOException exception) {
             throw new CompanyException("Could not parse company.", exception);
         }
-        LOGGER.info(() -> String.format(">>> Gets the data as of: '%s'.", element.text()));
+        LOGGER.info( String.format(">>> Gets the date as of: '%s'.", element.text()));
         return element.text();
     }
 }
