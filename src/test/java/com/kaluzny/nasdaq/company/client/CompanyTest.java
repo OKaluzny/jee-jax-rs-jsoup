d5d35f7493df967aef8e87857bcc00e3e64113c3
Oleg Kaluzny <cryoengineer@gmail.com>
2019-11-15 10:59:40 +0200

    Updated some tests.

--- src/test/java/com/kaluzny/nasdaq/company/client/CompanyTest.java
+++ src/test/java/com/kaluzny/nasdaq/company/client/CompanyTest.java
@@ -3,42 +3,54 @@ package com.kaluzny.nasdaq.company.client;
 import com.kaluzny.nasdaq.company.Company;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class CompanyTest {
 
+    private static final String EXCHANGE = "Exchange: NASDAQ";
+    private static final String SECTOR_COMPANY = "Industry: Technology";
+    private static final String COMPANY = "AAPL";
+    private static final String EXCHANGE_PRICE = "$100";
+    private static final String MARKET_TIME = "8/23/2016";
+
     @Test
     public void testSetExchange() throws Exception {
         Company company = new Company();
-        company.setExchange("Exchange: NASDAQ");
-        assertTrue(company.getExchange() == "Exchange: NASDAQ");
+        company.setExchange(EXCHANGE);
+        assertTrue(company.getExchange().equalsIgnoreCase(EXCHANGE));
+        assertFalse(SECTOR_COMPANY, false);
     }
 
     @Test
     public void testSetSectorCompany() throws Exception {
         Company company = new Company();
-        company.setSectorCompany("Industry: Technology");
-        assertTrue(company.getSectorCompany() == "Industry: Technology");
+        company.setSectorCompany(SECTOR_COMPANY);
+        assertTrue(company.getSectorCompany().equalsIgnoreCase(SECTOR_COMPANY));
+        assertFalse(EXCHANGE, false);
     }
 
     @Test
     public void testSetSymbolCompany() throws Exception {
         Company company = new Company();
-        company.setSymbolCompany("AAPL");
-        assertTrue(company.getSymbolCompany() == "AAPL");
+        company.setSymbolCompany(COMPANY);
+        assertTrue(company.getSymbolCompany().equalsIgnoreCase(COMPANY));
+        assertFalse(SECTOR_COMPANY, false);
     }
 
     @Test
     public void testSetExchangePrice() throws Exception {
         Company company = new Company();
-        company.setExchangePrice("$100");
-        assertTrue(company.getExchangePrice() == "$100");
+        company.setExchangePrice(EXCHANGE_PRICE);
+        assertTrue(company.getExchangePrice().equalsIgnoreCase(EXCHANGE_PRICE));
+        assertFalse(SECTOR_COMPANY, false);
     }
 
     @Test
     public void testSetDataAsOf() throws Exception {
         Company company = new Company();
-        company.setMarketTime("8/23/2016");
-        assertTrue(company.getMarketTime() == "8/23/2016");
+        company.setMarketTime(MARKET_TIME);
+        assertTrue(company.getMarketTime().equalsIgnoreCase(MARKET_TIME));
+        assertFalse(SECTOR_COMPANY, false);
     }
 }
