56acd806f632eb6d22d6a2d9a8b8d00e0664f209
Oleg Kaluzny <cryoengineer@gmail.com>
2019-11-15 12:35:22 +0200

    Updated some tests.

--- src/test/java/com/kaluzny/CompanyServiceTest.java
+++ src/test/java/com/kaluzny/CompanyServiceTest.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
+
 
 public class CompanyServiceTest {
 
@@ -20,51 +22,52 @@ public class CompanyServiceTest {
 
     @Test
     public void testGetDocument() throws IOException {
-        CompanyService spyParser = mock(CompanyService.class);
+        CompanyService spyParser = spy(CompanyService.class);
         doReturn(document).when(spyParser).getDocument(any());
         spyParser.getDocument(COMPANY);
         verify(spyParser, times(1)).getDocument(COMPANY);
         assertEquals(document, spyParser.getDocument(COMPANY));
-        assertNotEquals("AAPL", spyParser.getDocument(COMPANY));
+        assertNotEquals(COMPANY, spyParser.getDocument(COMPANY));
     }
 
     @Test
     public void testGetExchange() throws IOException {
-        CompanyService spyParser = mock(CompanyService.class);
+        CompanyService spyParser = spy(CompanyService.class);
         doReturn(COMPANY).when(spyParser).getExchange(any());
         spyParser.getExchange(COMPANY);
         verify(spyParser, times(1)).getExchange(COMPANY);
-        assertEquals("AAPL", spyParser.getExchange(COMPANY));
+        assertEquals(COMPANY, spyParser.getExchange(COMPANY));
         assertNotEquals("Alfa", spyParser.getExchange(COMPANY));
     }
 
     @Test
     public void testGetSectorCompany() throws IOException {
-        CompanyService spyParser = mock(CompanyService.class);
+        CompanyService spyParser = spy(CompanyService.class);
         doReturn(COMPANY).when(spyParser).getSectorCompany(any());
         spyParser.getSectorCompany(COMPANY);
         verify(spyParser, times(1)).getSectorCompany(COMPANY);
-        assertEquals("AAPL", spyParser.getSectorCompany(COMPANY));
+        assertEquals(COMPANY, spyParser.getSectorCompany(COMPANY));
         assertNotEquals("Alfa", spyParser.getSectorCompany(COMPANY));
     }
 
     @Test
     public void testGetExchangePrice() throws IOException {
-        CompanyService spyParser = mock(CompanyService.class);
+        CompanyService spyParser = spy(CompanyService.class);
         doReturn(COMPANY).when(spyParser).getExchangePrice(any());
         spyParser.getExchangePrice(COMPANY);
         verify(spyParser, times(1)).getExchangePrice(COMPANY);
-        assertEquals("AAPL", spyParser.getExchangePrice(COMPANY));
+        assertEquals(COMPANY, spyParser.getExchangePrice(COMPANY));
         assertNotEquals("Alfa", spyParser.getExchangePrice(COMPANY));
     }
 
     @Test
     public void testGetMarketTime() throws IOException {
-        CompanyService spyParser = mock(CompanyService.class);
+        CompanyService spyParser = spy(CompanyService.class);
         doReturn(COMPANY).when(spyParser).getMarketTime(any());
         spyParser.getMarketTime(COMPANY);
         verify(spyParser, times(1)).getMarketTime(COMPANY);
-        assertEquals("AAPL", spyParser.getMarketTime(COMPANY));
+        assertEquals(COMPANY, spyParser.getMarketTime(COMPANY));
         assertNotEquals("Alfa", spyParser.getMarketTime(COMPANY));
+        doThrow(new RuntimeException()).when(spyParser).getMarketTime(any());
     }
 }
