16eb6b99f85d8e98badbf26818da0f75214c0d28
Oleg Kaluzny <cryoengineer@gmail.com>
2019-11-18 16:50:48 +0200

    Changed url

--- src/main/java/com/kaluzny/nasdaq/company/CompanyService.java
+++ src/main/java/com/kaluzny/nasdaq/company/CompanyService.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import java.io.IOException;
@@ -16,9 +14,8 @@ import java.io.IOException;
 @Slf4j
 public class CompanyService {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(CompanyService.class);
-    private static final String URL_FORMAT = "http://www.nasdaq.com/symbol/%s/real-time";
-    private static final String USER_AGENT = "Chrome";
+    private static final String URL_FORMAT = "https://old.nasdaq.com/symbol/%s/real-time";
+    private static final String USER_AGENT = "chrome";
     private static final String EXCHANGE_LABEL = "qbar_exchangeLabel";
     private static final String SECTOR_LABEL = "qbar_sectorLabel";
     private static final String DWIDGET_DOLLAR = "qwidget-dollar";
@@ -29,76 +26,76 @@ public class CompanyService {
 
     @Logged
     public Document getDocument(String company) throws IOException {
-        log.trace("getDocument() - start: company = {}", company);
+        log.info("getDocument() - start: company = {}", company);
         String url = String.format(URL_FORMAT, company);
         document = Jsoup.connect(url).userAgent(USER_AGENT).get();
-        log.trace("getDocument() - end: document = {}", document);
+        log.info("getDocument() - end: document = {}", document);
         return document;
     }
 
     @Logged
     public String getExchange(String company) {
-        log.trace("getExchange() - start: company = {}", company);
+        log.info("getExchange() - start: company = {}", company);
         try {
             document = getDocument(company);
             element = document.getElementById(EXCHANGE_LABEL);
-            log.trace("getExchange() - end: document = {}, element = {}", document, element);
+            log.info("getExchange() - end: document = {}, element = {}", document, element);
         } catch (IOException exception) {
             throw new CompanyException("Could not parse an exchange.", exception);
         }
-        LOGGER.info(String.format(">>> Gets the name an exchange: '%s'.", element.text()));
+        log.info(String.format(">>> Gets the name an exchange: '%s'.", element.text()));
         return element.text();
     }
 
-    /*catch (NullPointerException ex) {
-        String errorMessage = String.format(" with id=%s is not found", id);
-        log.error(errorMessage, ex);
-        throw new EntityNotFoundException(errorMessage);*/
-
     @Logged
     public String getSectorCompany(String company) {
+        log.info("getSectorCompany() - start: company = {}", company);
         try {
             document = getDocument(company);
             element = document.getElementById(SECTOR_LABEL);
         } catch (IOException exception) {
             throw new CompanyException("Could not parse sector.", exception);
         }
-//        LOGGER.info(String.format(">>> Gets the name a sector: '%s'.", element.text()));
+        log.info(String.format(">>> Gets the name a sector: '%s'.", element.text()));
         return element.text();
     }
 
     @Logged
     public String getExchangePrice(String company) {
+        log.info("getExchangePrice() - start: company = {}", company);
         try {
             document = getDocument(company);
             element = document.getElementsByClass(DWIDGET_DOLLAR).first();
         } catch (IOException exception) {
             throw new CompanyException("Could not parse company.", exception);
         }
-        LOGGER.info(String.format(">>> Gets the stock quotes: '%s'.", element.text()));
+        log.info(String.format(">>> Gets the stock quotes: '%s'.", element.text()));
         return element.text();
     }
 
     @Logged
     public String getMarketTime(String company) {
+        log.info("getMarketTime() - start: company = {}", company);
         try {
             document = getDocument(company);
             element = document.getElementById(DWIDGET_MARKETTIME);
         } catch (IOException exception) {
             throw new CompanyException("Could not parse company.", exception);
         }
-        LOGGER.info(String.format(">>> Gets the date as of: '%s'.", element.text()));
+        log.info(String.format(">>> Gets the date as of: '%s'.", element.text()));
         return element.text();
     }
 
     @Logged
     public Company buildCompany(String companySymbol) {
+        log.info("buildCompany() - start: companySymbol = {}", companySymbol);
         Company company = new Company();
         company.setSymbolCompany(companySymbol);
         company.setExchange(getExchange(companySymbol));
         company.setSectorCompany(getSectorCompany(companySymbol));
         company.setExchangePrice(getExchangePrice(companySymbol));
         company.setMarketTime(getMarketTime(companySymbol));
+        log.info("buildCompany() - end: company = {}", company);
         return company;
     }
 }
